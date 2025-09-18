6e8f0e8314ee7c3d84af4b561c7ec95edc81956e
dimene <ugrumydm@gmail.com>
2025-09-18 17:33:58 +0300

    deleted useless files

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# обновляем pip и setuptools
-pip install --upgrade pip setuptools wheel
-pip install --no-cache-dir cython
-pip install --no-cache-dir --only-binary=:all: lightfm
-pip install --no-cache-dir --only-binary=:all: nmslib-metabrainz-2.1.3
-pip install -r requirements.txt
